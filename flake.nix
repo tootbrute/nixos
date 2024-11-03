857dc1faa1cd6f5aabd49f70ba8f48acdd614673
tootbrute <tootbrute@tutanota.com>
2024-11-03 19:28:23 +0800

    update

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@ in
     (import "${home-manager}/nixos")
   ];
 
-  home-manager.users.my_username = {
+  home-manager.users.elias = {
     /* The home.stateVersion option does not have a default and must be set */
     home.stateVersion = "24.05";
     /* Here goes the rest of your home-manager config, e.g. home.packages = [ pkgs.foo ]; */
