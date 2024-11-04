819dfe3e35725a51a6962daef7f904fe55192da2
tootbrute <tootbrute@tutanota.com>
2024-11-04 17:23:23 +0800

    home-manager working!

--- configuration.nix
+++ configuration.nix
@@ -147,7 +147,8 @@
     corefonts  
     vistafonts  
     noto-fonts  
-    noto-fonts-cjk  
+    noto-fonts-cjk-sans
+    noto-fonts-cjk-serif  
     noto-fonts-emoji  
     liberation_ttf  
     fira-code  
