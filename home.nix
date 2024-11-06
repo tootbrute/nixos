667ec6cb8f380cd3485c3a5ed567a9554134dec9
tootbrute <tootbrute@tutanota.com>
2024-11-06 16:40:44 +0800

    update

--- home.nix
+++ home.nix
@@ -115,7 +115,8 @@
       env.TERM = "xterm-256color";
       font = {
         size = 12;
-        draw_bold_text_with_bright_colors = true;
+        # this causes error
+        #draw_bold_text_with_bright_colors = true;
       };
       scrolling.multiplier = 5;
       selection.save_to_clipboard = true;
